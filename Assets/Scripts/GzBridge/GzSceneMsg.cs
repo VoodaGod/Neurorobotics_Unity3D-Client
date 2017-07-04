350c7e50bb684d5d7aca40fd5a08ca36c548f102
Sandro Weber <webers@in.tum.de>
2017-07-04 17:59:20 +0200

    [NRRSCI-247] starting to build scene from gztopic ~/scene

---
+++ Assets/Scripts/GzBridge/GzSceneMsg.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using SimpleJSON;
+using UnityEngine;
+
+public class GzSceneMsg : ROSBridgeMsg
+{
+
+    #region PUBLIC_MEMBER_VARIABLES
+
+    public JSONNode MsgJSON
+    {
+        get
+        {
+            return this.msg_json_;
+        }
+
+        set
+        {
+            this.msg_json_ = value;
+        }
+    }
+
+    #endregion //PUBLIC_MEMBER_VARIABLES
+
+    #region PRIVATE_MEMBER_VARIABLES
+
+    private JSONNode msg_json_;
+
+    #endregion //PRIVATE_MEMBER_VARIABLES
+
+    #region MONOBEHAVIOR_METHODS
+    #endregion //MONOBEHAVIOR_METHODS
+
+    #region PUBLIC_METHODS
+
+    public GzSceneMsg(JSONNode msg)
+    {
+        this.msg_json_ = msg;
+    }
+
+    public static string GetMessageType()
+    {
+        return "gazebo.msgs.Scene";
+    }
+
+    public override string ToString()
+    {
+        return "gazebo.msgs.Scene [name =";
+    }
+
+    public override string ToYAMLString()
+    {
+        return "{}";
+    }
+
+    #endregion //PUBLIC_METHODS
+
+    #region PRIVATE_METHODS
+    #endregion //PRIVATE_METHODS
+}
---
+++ Assets/Scripts/GzBridge/GzSceneTopicSubscriber.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using ROSBridgeLib;
+using SimpleJSON;
+using UnityEngine;
+
+public class GzSceneTopicSubscriber : ROSBridgeSubscriber
+{
+    #region PUBLIC_MEMBER_VARIABLES
+    #endregion //PUBLIC_MEMBER_VARIABLES
+
+    #region PRIVATE_MEMBER_VARIABLES
+    #endregion //PRIVATE_MEMBER_VARIABLES
+
+    #region MONOBEHAVIOR_METHODS
+    #endregion //MONOBEHAVIOR_METHODS
+
+    #region PUBLIC_METHODS
+
+    public new static string GetMessageTopic()
+    {
+        return "~/scene";
+    }
+
+    public new static string GetMessageType()
+    {
+        return "gazebo.msgs.Scene";
+    }
+
+    public new static ROSBridgeMsg ParseMessage(JSONNode msg)
+    {
+        Debug.Log("GzSceneTopicSubscriber.ParseMessage()");
+        return new GzSceneMsg(msg);
+    }
+
+    public new static void CallBack(ROSBridgeMsg msg)
+    {
+        Debug.Log("GzSceneTopicSubscriber.CallBack()");
+        //GzSceneMsg scene_msg = (GzSceneMsg)msg;
+        
+        GzBridgeManager.Instance.ReceiveMessage((GzSceneMsg)msg);
+    }
+
+    #endregion //PUBLIC_METHODS
+
+    #region PRIVATE_METHODS
+
+    #endregion //PRIVATE_METHODS
+}
