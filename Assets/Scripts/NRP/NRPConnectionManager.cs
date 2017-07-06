5257f6eda5487387d512d170305ce030a264da08
Sandro Weber <webers@in.tum.de>
2017-07-06 13:56:39 +0200

    before rebase

---
+++ Assets/Scripts/NRP/NRPConnectionManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NRPConnectionManager : MonoBehaviour {
+
+    public string NRPBackendIP = "192.168.0.153";
+
+    private GzBridgeManager gzBridgeManager;
+    private ROSBridgeManager rosBridgeManager;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
