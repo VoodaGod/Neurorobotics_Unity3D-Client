668ecccebd10ba22f5f60cd20ae8e6fcb32c9753
Sandro Weber <webers@in.tum.de>
2017-07-06 16:22:23 +0200

    basic geometry creation

--- Assets/Scripts/Gazebo/GazeboSceneManager.cs
+++ Assets/Scripts/Gazebo/GazeboSceneManager.cs
@@ -55,48 +55,50 @@ public class GazeboSceneManager : MonoBehaviour {
         }
 
         // lights
+        GameObject lights_parent = new GameObject("lights");
+        lights_parent.transform.parent = this.gameObject.transform;
+
         JSONArray lights = sceneMsg["light"].AsArray;
         foreach(JSONNode light in lights)
         {
-            this.CreateLightFromJSON(light);
+            this.CreateLightFromJSON(light, lights_parent.transform);
         }
 
         // models
+        GameObject models_parent = new GameObject("models");
+        models_parent.transform.parent = this.gameObject.transform;
+
         JSONArray models = sceneMsg["model"].AsArray;
         foreach (JSONNode model in models)
         {
-            this.CreateModelFromJSON(model);
+            this.CreateModelFromJSON(model, models_parent.transform);
         }
 
         // joints
+        GameObject joints_parent = new GameObject("joints");
+        joints_parent.transform.parent = this.gameObject.transform;
+
         JSONArray joints = sceneMsg["joint"].AsArray;
         foreach (JSONNode joint in joints)
         {
-            this.CreateJointFromJSON(joint);
+            this.CreateJointFromJSON(joint, joints_parent.transform);
         }
 
         return true;
     }
 
-    private void CreateLightFromJSON(JSONNode json_light)
+    private void CreateLightFromJSON(JSONNode json_light, Transform parent_transform)
     {
         string light_name = json_light["name"];
         GameObject light_gameobject = new GameObject(light_name);
-        light_gameobject.transform.parent = this.gameObject.transform;
+        light_gameobject.transform.parent = parent_transform;
         Light light_component = light_gameobject.AddComponent<Light>();
 
         // pose
         JSONNode json_pose = json_light["pose"];
         if (json_pose != null)
         {
-            // position
-            JSONNode json_position = json_pose["position"];
-            Vector3 position = new Vector3(json_position["x"].AsFloat, json_position["y"].AsFloat, json_position["z"].AsFloat);
-            light_gameobject.transform.position = Gz2UnityVec3(position);
-            // rotation
-            JSONNode json_rotation = json_pose["orientation"];
-            Quaternion rotation = new Quaternion(json_rotation["x"].AsFloat, json_rotation["y"].AsFloat, json_rotation["z"].AsFloat, json_rotation["w"].AsFloat);
-            light_gameobject.transform.rotation = Gz2UnityQuaternion(rotation);
+            this.SetPoseFromJSON(json_pose, light_gameobject);
         }
 
         // color
@@ -163,14 +165,164 @@ public class GazeboSceneManager : MonoBehaviour {
         }
     }
 
-    private void CreateModelFromJSON(JSONNode model_node)
+    private void CreateModelFromJSON(JSONNode json_model, Transform parent_transform)
+    {
+        string model_name = json_model["name"];
+        GameObject model_gameobject = new GameObject(model_name);
+        model_gameobject.transform.SetParent(parent_transform, false);
+
+        // pose
+        JSONNode json_pose = json_model["pose"];
+        if (json_pose != null)
+        {
+            this.SetPoseFromJSON(json_pose, model_gameobject);
+        }
+
+        // links
+        GameObject links_parent = new GameObject("links");
+        links_parent.transform.SetParent(model_gameobject.transform, false);
+
+        JSONArray links = json_model["link"].AsArray;
+        foreach (JSONNode link in links)
+        {
+            float model_scale = json_model["scale"].AsFloat;
+            this.CreateLinkFromJSON(link, links_parent.transform, model_scale);
+        }
+    }
+
+    private void CreateLinkFromJSON(JSONNode json_link, Transform parent_transform, float model_scale)
+    {
+        string link_name = json_link["name"];
+        GameObject link_gameobject = new GameObject(link_name);
+        link_gameobject.transform.SetParent(parent_transform, false);
+
+        // pose
+        JSONNode json_pose = json_link["pose"];
+        if (json_pose != null)
+        {
+            this.SetPoseFromJSON(json_pose, link_gameobject);
+        }
+
+        // visuals
+        GameObject visuals_parent = new GameObject("visuals");
+        visuals_parent.transform.SetParent(link_gameobject.transform, false);
+
+        JSONArray visuals = json_link["visual"].AsArray;
+        foreach (JSONNode visual in visuals)
+        {
+            this.CreateVisualFromJSON(visual, visuals_parent.transform, model_scale);
+        }
+
+        // collisions
+        GameObject collisions_parent = new GameObject("collisions");
+        collisions_parent.transform.SetParent(link_gameobject.transform, false);
+
+        JSONArray collisions = json_link["collision"].AsArray;
+        foreach (JSONNode collision in collisions)
+        {
+            this.CreateVisualFromJSON(collision, collisions_parent.transform, model_scale);
+        }
+
+        // sensors
+        GameObject sensors_parent = new GameObject("sensors");
+        sensors_parent.transform.SetParent(link_gameobject.transform, false);
+
+        JSONArray sensors = json_link["sensor"].AsArray;
+        foreach (JSONNode sensor in sensors)
+        {
+            this.CreateSensorFromJSON(sensors, sensors_parent.transform);
+        }
+    }
+
+    private void CreateVisualFromJSON(JSONNode json_visual, Transform parent_transform, float model_scale)
+    {
+        JSONNode json_geometry = json_visual["geometry"];
+        if (json_geometry != null)
+        {
+            string visual_name = json_visual["name"];
+            GameObject visual_gameobject = new GameObject(visual_name);
+            visual_gameobject.transform.SetParent(parent_transform, false);
+
+            // pose
+            JSONNode json_pose = json_visual["pose"];
+            if (json_pose != null)
+            {
+                this.SetPoseFromJSON(json_pose, visual_gameobject);
+            }
+
+            // geometry
+            JSONNode json_material = json_visual["material"];
+            this.CreateGeometryFromJSON(json_geometry, json_material, visual_gameobject.transform, model_scale);
+
+            // cast shadows
+            bool json_cast_shadows = json_visual["cast_shadows"].AsBool;
+            if (json_cast_shadows)
+            {
+                
+            }
+
+            // receive shadows
+        }
+    }
+
+    private void CreateGeometryFromJSON(JSONNode json_geometry, JSONNode json_material, Transform parent_transform, float model_scale)
+    {
+        if (json_geometry["box"] != null)
+        {
+            GameObject geometry_gameobject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            geometry_gameobject.transform.SetParent(parent_transform, false);
+
+            JSONNode json_size = json_geometry["box"].AsObject["size"];
+            Vector3 local_scale = new Vector3(json_size["x"].AsFloat, json_size["y"].AsFloat, json_size["z"].AsFloat) / model_scale;
+            //geometry_gameobject.transform.localScale = local_scale;
+            //TODO: fix
+        }
+        else if (json_geometry["cylinder"] != null)
+        {
+            GameObject geometry_gameobject = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            geometry_gameobject.transform.SetParent(parent_transform, false);
+        }
+        else if (json_geometry["sphere"] != null)
+        {
+            GameObject geometry_gameobject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            geometry_gameobject.transform.SetParent(parent_transform, false);
+        }
+        else if (json_geometry["plane"] != null)
+        {
+            GameObject geometry_gameobject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            geometry_gameobject.transform.SetParent(parent_transform, false);
+        }
+        else if (json_geometry["mesh"] != null)
+        {
+
+        }
+    }
+
+    private void CreateCollisionFromJSON(JSONNode json_collision, Transform parent_transform)
     {
 
     }
 
-    private void CreateJointFromJSON(JSONNode joint_node)
+    private void CreateSensorFromJSON(JSONNode json_sensor, Transform parent_transform)
     {
 
+    }
+
+    private void CreateJointFromJSON(JSONNode json_joint, Transform parent_transform)
+    {
+
+    }
+
+    private void SetPoseFromJSON(JSONNode json_pose, GameObject gameobject)
+    {
+        // position
+        JSONNode json_position = json_pose["position"];
+        Vector3 position = new Vector3(json_position["x"].AsFloat, json_position["y"].AsFloat, json_position["z"].AsFloat);
+        gameobject.transform.localPosition = Gz2UnityVec3(position);
+        // rotation
+        JSONNode json_rotation = json_pose["orientation"];
+        Quaternion rotation = new Quaternion(json_rotation["x"].AsFloat, json_rotation["y"].AsFloat, json_rotation["z"].AsFloat, json_rotation["w"].AsFloat);
+        gameobject.transform.localRotation = Gz2UnityQuaternion(rotation);
     }
 
     #region Convert function from gazebo to unity and vice versa.
@@ -179,7 +331,7 @@ public class GazeboSceneManager : MonoBehaviour {
     /// </summary>
     /// <param name="gazeboRot">Quaternion in gazebo coordinate frame.</param>
     /// <returns>Quaternion in unity coordinate frame.</returns>
-    Quaternion Gz2UnityQuaternion(Quaternion gazeboRot)
+    private Quaternion Gz2UnityQuaternion(Quaternion gazeboRot)
     {
         Quaternion rotX = Quaternion.AngleAxis(180f, Vector3.right);
         Quaternion rotZ = Quaternion.AngleAxis(180f, Vector3.forward);
@@ -196,7 +348,7 @@ public class GazeboSceneManager : MonoBehaviour {
     /// </summary>
     /// <param name="gazeboPos">Vector in gazebo coordinate frame.</param>
     /// <returns>Vector in unity coordinate frame.</returns>
-    Vector3 Gz2UnityVec3(Vector3 gazeboPos)
+    private Vector3 Gz2UnityVec3(Vector3 gazeboPos)
     {
         return new Vector3(gazeboPos.x, gazeboPos.z, gazeboPos.y);
     }
@@ -206,7 +358,7 @@ public class GazeboSceneManager : MonoBehaviour {
     /// </summary>
     /// <param name="unityPos">Vector in unity coordinate frame.</param>
     /// <returns>Vector in gazebo coordinate frame.</returns>
-    Vector3 Unity2GzVec3(Vector3 unityPos)
+    private Vector3 Unity2GzVec3(Vector3 unityPos)
     {
         return new Vector3(unityPos.x, unityPos.z, unityPos.y);
     }
@@ -216,7 +368,7 @@ public class GazeboSceneManager : MonoBehaviour {
     /// </summary>
     /// <param name="unityRot">Quaternion in unity coordinate frame.</param>
     /// <returns>Quaternion in gazebo coordinate frame.</returns>
-    Quaternion Unity2GzQuaternion(Quaternion unityRot)
+    private Quaternion Unity2GzQuaternion(Quaternion unityRot)
     {
         Quaternion rotX = Quaternion.AngleAxis(180f, Vector3.right);
         Quaternion rotZ = Quaternion.AngleAxis(180f, Vector3.forward);
