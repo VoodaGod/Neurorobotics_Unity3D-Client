dbee4a9a40f46214e3d8720e5f40bc1ae73ee821
Sandro Weber <webers@in.tum.de>
2018-11-23 15:07:03 +0100

    pid implemented

--- Assets/Scripts/Avatar/GzLinkLinearVelocityPID.cs
+++ Assets/Scripts/Avatar/GzLinkLinearVelocityPID.cs
@@ -6,16 +6,19 @@ using ROSBridgeLib.geometry_msgs;
 
 public class GzLinkLinearVelocityPID : MonoBehaviour {
 
+    public float p = 4f, i = 0.2f, d = 0.5f;
+
+    private Vector3 integral = new Vector3(), position_diff_previous = new Vector3();
+
     private bool active = false;
 
     private string topic, gz_link_name;
     private Transform rig_transform, gz_target_transform, gz_link_transform;
-    private float publish_rate = 0.01f;
+    private float publish_rate = 0.05f;
     private float t_last_publish;
 
     private Quaternion rotation_ybot_unity2gazebo;
-
-	// Use this for initialization
+    
 	void Start () {
         Matrix4x4 matrix_ybot_unity2gazebo = new Matrix4x4();
         matrix_ybot_unity2gazebo.SetRow(0, new Vector4(-1, 0, 0, 0));
@@ -26,8 +29,8 @@ public class GzLinkLinearVelocityPID : MonoBehaviour {
         this.rotation_ybot_unity2gazebo = matrix_ybot_unity2gazebo.rotation;
     }
 	
-	// Update is called once per frame
-	void Update () {
+	void FixedUpdate ()
+    {
         if (!this.active) return;
 
         float t = Time.time;
@@ -38,7 +41,12 @@ public class GzLinkLinearVelocityPID : MonoBehaviour {
             this.UpdateTargetFromRig();
 
             Vector3 position_diff = this.gz_target_transform.position - this.gz_link_transform.position;
-            Vector3 velocity = GazeboSceneManager.Unity2GzVec3(position_diff) * 5f;
+
+            this.integral = this.integral + position_diff * Time.fixedDeltaTime;
+            Vector3 derivative = (position_diff - this.position_diff_previous) / Time.fixedDeltaTime;
+            Vector3 pidOutput = p * position_diff + i * this.integral + d * derivative;
+
+            Vector3 velocity = GazeboSceneManager.Unity2GzVec3(pidOutput);
             Vector3Msg velocity_msg = new Vector3Msg(velocity.x, velocity.y, velocity.z);
             ROSBridgeService.Instance.websocket.Publish(topic, velocity_msg);
         }
