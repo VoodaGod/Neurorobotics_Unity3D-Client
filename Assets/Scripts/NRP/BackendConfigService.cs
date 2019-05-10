0e8a7e70ea6bbe7cc0e8afcf5c4545a48fa70ece
Sandro Weber <webers@in.tum.de>
2019-05-10 16:41:10 +0200

    extended rosbridge connection for docker setup

--- Assets/Scripts/NRP/BackendConfigService.cs
+++ Assets/Scripts/NRP/BackendConfigService.cs
@@ -6,7 +6,8 @@ public class BackendConfigService : Singleton<BackendConfigService>
 {
     public string IP = "xxx.xxx.xxx.xxx";
     public int GzBridgePort = 8080;
-    public int ROSBridgePort = 9090;
+    public int ROSBridgePort = 8080;
+    public string ROSBridgeSuffix = "/rosbridge";
     public int ProxyPort = 9000;
 
     private ROSBridge ROSBridgeManager;
@@ -14,27 +15,10 @@ public class BackendConfigService : Singleton<BackendConfigService>
 	// Use this for initialization
 	void Start ()
     {
-        if (!string.IsNullOrEmpty(this.IP))
-        {
-            //this.ConnectToROSBridge();
-        }
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-    }
-
-    private void ConnectToROSBridge()
-    {
-        // initialize ROSBridge component
-        if (this.gameObject.GetComponent<ROSBridge>() == null)
-        {
-            this.gameObject.AddComponent<ROSBridge>();
-        }
-        this.ROSBridgeManager = this.gameObject.GetComponent<ROSBridge>();
-
-        ROSBridgeManager.ROSCoreIP = this.IP;
-        ROSBridgeManager.Port = ROSBridgePort;
     }
 }
