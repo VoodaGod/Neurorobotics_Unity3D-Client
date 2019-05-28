89f4380127154ba5eb7a7d2649da06a4f6a0bff5
VoodaGod <l33tjas.0n@gmail.com>
2019-05-28 23:41:20 +0200

    add basic fade to black effect for head discrepancy

--- Assets/Scripts/Embodiment/DiscrepancyHandler.cs
+++ Assets/Scripts/Embodiment/DiscrepancyHandler.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,6 +10,12 @@ public class DiscrepancyHandler : MonoBehaviour {
 	public bool lineEffectEnabled = true;
 	public bool lineEffectHands = true;
 	public bool lineEffectFeet = true;
+
+	[SerializeField]
+	SteamVR_Camera steamvrCam;
+	public bool fadeToBlackEffect = true;
+	[SerializeField]
+	float headMaxDistance = 0.3f;
 	Dictionary<DiscrepancyTracker.TrackedJoints, DiscrepancyLine> lineDict = new Dictionary<DiscrepancyTracker.TrackedJoints, DiscrepancyLine>();
 
 	public void HandleDiscrepancy(DiscrepancyTracker.TrackedJoints joint, Transform local, Transform remote, float distance)
@@ -35,6 +41,15 @@ public class DiscrepancyHandler : MonoBehaviour {
 				lineDict[joint].DrawLine(local, remote, distance);
 			}
 		}
+
+		if (joint == DiscrepancyTracker.TrackedJoints.Head)
+		{
+			if (fadeToBlackEffect)
+			{
+				float alpha = Mathf.Lerp(0, 1, distance / headMaxDistance);
+				SteamVR_Fade.Start(new Color(0, 0, 0, alpha), 0);
+			}
+		}
 	}
 
 	public void StopHandlingDiscrepancyHand(DiscrepancyTracker.TrackedJoints joint)
@@ -50,6 +65,17 @@ public class DiscrepancyHandler : MonoBehaviour {
 	{
 		if (DiscrepancyLinePrefab == null){
 			Debug.LogError("DiscrepancyLinePrefab not set");
+		}
+
+		if (steamvrCam == null)
+		{
+			steamvrCam = GameObject.FindObjectOfType<SteamVR_Camera>();
+			if (steamvrCam == null){
+				Debug.LogError("no SteamVR_Camera found");
+			}
+			if (steamvrCam.gameObject.GetComponent<SteamVR_Fade>() == null){
+				steamvrCam.gameObject.AddComponent<SteamVR_Fade>();
+			}
 		}
 	}
 	
